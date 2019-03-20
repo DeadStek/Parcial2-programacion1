1f6a90e57ecd09d68b790a99c70b6275f13e4847
DeadStek <48686765+DeadStek@users.noreply.github.com>
2019-03-19 19:57:22 -0600

    Add files via upload

---
+++ Parcial2/Class2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    class Class2
+    {
+        int Num1, num2, Edad;
+        String Usuario, Alumno;
+
+        public void Metodo1()
+        {
+            Console.WriteLine("Digite numero 1 o Numero 0:");
+        }
+        public void Metodo2()
+        {
+            Console.WriteLine("Digite numero 2:");
+        }
+
+        public void Metodo()
+        {
+            Console.WriteLine("Digite 3 para capturar ");
+
+        }
+
+
+        public void Capturar()
+        {
+            Console.WriteLine("Digite 3 para mostrar nombre");
+
+           
+
+        }
+
+        public void Edades()
+        {
+            Console.WriteLine("Digite 4 para mostrar 5 edades");
+
+        }
+    }
+}
